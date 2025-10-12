82b872b9921321d1859f29a7717d0af174800de4
locbones <41172978+locbones@users.noreply.github.com>
2025-10-11 18:37:06 -0700

    0.9.9.9GAH

--- RMD-MP.mpq/data/D2RLAN/Filters/lootfilter_default.lua
+++ RMD-MP.mpq/data/D2RLAN/Filters/lootfilter_default.lua
@@ -12,7 +12,7 @@ return {
         { --Display item levels for weapons, armors, charms, jewels, rings, amulets and arrows, in white, to the right of item name, (x)
             codes = "allitems",
             location = { "onground", "onplayer", "equipped", "atvendor" },
-            itype = { 5, 10, 12, 45, 50, 58, 82, 83, 84 },
+            itype = { 5, 6, 10, 12, 45, 50, 58, 82, 83, 84 },
             suffix = " {white}({ilvl})",
         },
         { --Display socket count in gray, to the right of item name, [x]
@@ -23,17 +23,15 @@ return {
             suffix = " {gray}[{sockets}]"
         },
         { --Display RMD "Elite Bases" with a special EB icon in RMD Red, to the right of item name
-            codes = "allitems",
+            codes = NOT { "l01", "l02", "l03", "l04", "l05", "l06", "l07", "l08", "l09", "l10", "l11", "l12", "l13", "l14","l15", "l16", "l17", "l18" },
             rarity = 2,
             location = { "onground", "onplayer", "equipped", "atvendor" },
-            ilvl = "102-",
             itype = { 45, 50 },
             suffix = " {tan}ⅲ"
         },
         { --Display RMD "Limit Broken Bases" with a special LB icon in RMD Red, to the right of item name
             codes = { "l01", "l02", "l03", "l04", "l05", "l06", "l07", "l08", "l09", "l10", "l11", "l12", "l13", "l14","l15", "l16", "l17", "l18" },
             location = { "onground", "onplayer", "equipped", "atvendor" },
-            ilvl = "103+",
             itype = { 45, 50 },
             suffix = " {tan}ⅳ"
         },
